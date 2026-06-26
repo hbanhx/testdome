450ce7925fa4827ead165b6538deefa79df0bac8
hbanhx <51317341+hbanhx@users.noreply.github.com>
2026-06-26 13:53:00 +0200

    .

--- parking_slots.py
+++ parking_slots.py
@@ -2,25 +2,25 @@ def get_minimum_slots(large_slots, small_slots, buses, cars):
     lots_used = 0
     remaining_large_slots = large_slots
     remaining_small_slots = small_slots
-    remaining_buses = 0
-    remaining_cars = 0
+    remaining_buses = buses
+    remaining_cars = cars
 
     # fit buses - if too many buses then return -1
-    if large_slots >= buses:
-        remaining_large_slots = large_slots - buses
-        lots_used += large_slots - buses
+    if remaining_large_slots >= remaining_buses:
+        remaining_large_slots -= remaining_buses
+        lots_used += remaining_buses
     else:
         return -1
     
     if remaining_large_slots > 0:
-            cars_in_large_slots = cars // 3
-            
-            remaining_large_slots > ( cars // 3):
-            lots_used += remaining_large_slots
-            remaining_cars = remaining_large_slots % 3
+        remaining_cars = remaining_cars - (remaining_large_slots * 3)
+        
 
-        if small_slots > remaining_cars:
-            lots_used += remaining_small_slots / cars
+        lots_used += remaining_large_slots 
+        remaining_cars = remaining_cars % 3
+
+        if remaining_small_slots > remaining_cars:
+            lots_used += remaining_small_slots // remaining_cars
         else:
             return -1
     
